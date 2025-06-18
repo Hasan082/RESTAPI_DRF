da13895881e03953a40299a71512fe9f32e9c4a6
Md Hasanuzzaman <dr.has82@gmail.com>
2025-06-18 23:32:52 +0600

    Run first test case

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class StatusAPITestCase(TestCase):
+    """
+    This class contains tests for the status list API endpoint.
+    Args:
+        TestCase (_type_): _description_
+    """
+    def test_status_list(self):
+        """
+        Test the status list API endpoint.
+        """
+        response = self.client.get('/api/status/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json(), list)
+        
+        
+
+
+
