1a832e059049e59a0b5e9864914f12f158b315df
Md Hasanuzzaman <dr.has82@gmail.com>
2025-06-18 23:56:45 +0600

    Import model

--- api/tests.py
+++ api/tests.py
@@ -17,6 +17,20 @@ class StatusAPITestCase(TestCase):
         self.assertIsInstance(response.json(), list)
         
         
-
-
-
+    def test_status_create(self):
+        """
+        Test the status create API endpoint.
+        """
+        data = {
+            'text': 'Test status',
+            'image_link': 'http://example.com/image.jpg',
+            'is_active': True,
+            'is_private': False
+        }
+        
+        response = self.client.post('/api/status/create/', data)
+        print(response.status_code)
+        print(response.json())
+        # self.assertEqual(response.status_code, 201)
+        self.assertIn('id', response.json())
+        self.assertEqual(response.json()['text'], data['text'])
